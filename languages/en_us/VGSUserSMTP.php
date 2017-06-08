559d5b8e968a0d1a63b48414300fbc7248eb1eca
Rafasama <rrc_o_bom@hotmail.com>
2017-06-08 13:30:47 -0300

    Sem alteração

---
+++ languages/en_us/VGSUserSMTP.php
@@ -0,0 +1,37 @@
+<?php
+/*+***********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ *************************************************************************************/
+$languageStrings = array(
+    'LBL_SMTP_CONFIG' => 'SMTP Config',
+    'LBL_SERVER_NAME' => 'Server Name',
+    'LBL_USER_LABEL' => 'User Name',
+    'LBL_PASSWORD' => 'Password',
+    'LBL_EMAIL_FROM' => 'Email From',
+    'LBL_FROM_NAME' => 'From Name',
+    'LBL_SMTP_AUTH' => 'SMTP Auth',
+    'ACTIONS' => 'Actions',
+    'SAVE' => 'Save',
+    'LBL_USER_NAME' => 'User Name',
+    'ADD_NEW' => 'Add New',
+    'ALREADY_SETTINGS' => 'SMTP Entry for the user already exists.',
+    'LBL_DB_INSERT_FAIL' => 'Settings could not be saved'
+);
+
+$jsLanguageStrings = array(
+	//Curency seperator validation messages
+	'JS_DECIMAL_SEPERATOR_AND_GROUPING_SEPERATOR_CANT_BE_SAME' => 'Decimal seperator and Grouping seperator cant be same',
+	'JS_ENTER_CONFIRMATION_PASSWORD'=>'Please enter your password confirmation.',
+	'JS_ENTER_NEW_PASSWORD'=>'Please enter your new password.',
+	'JS_ENTER_OLD_PASSWORD'=>'Please enter your old password.',
+	'JS_INVALID_PASSWORD'=>'You must specify a valid username and password.',
+	'JS_PASSWORD_CHANGE_FAILED_1'=>'User password change failed for ',
+	'JS_PASSWORD_CHANGE_FAILED_2'=>' failed.  The new password must be set.',
+	'JS_PASSWORD_INCORRECT_OLD'=>'Incorrect old password specified. Re-enter password information.',
+	'JS_REENTER_PASSWORDS'=>'Please re-enter passwords.  The \"new password\" and \"confirm password\" values do not match.',
+);
